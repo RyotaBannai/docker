168095752f09858fd31f9c55ff28192f54eafa79
RyotaBannai <ryotala0528@gmail.com>
2020-02-18 18:51:19 +0900

    Learnt how to use serverside launguage to construct  server in virual os with go and docker.

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
-FROM centos:centos7
+FROM golang:1.9
+RUN mkdir /echo
+COPY main.go /echo
+CMD ["go", "run", "/echo/main.go"]
 MAINTAINER Ryota BANNAI ryotabannai0528@gmail.com
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        // クライアントからリクエストを受けた場合
+        log.Println("received request")
+        // HTTPリクエストを受けた場合
+        fmt.Fprintf(w, "Hello Docker!!")
+    })
+
+    log.Println("start server")
+    server := &http.Server{Addr: ":8081"}
+    if err := server.ListenAndServe(); err != nil {
+        log.Println(err)
+    }
+}
